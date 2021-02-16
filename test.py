b65cc84b363a0273a536b49d835249754b624ab3
Nodari Pylypyshak <32438959+nodari-dev@users.noreply.github.com>
2021-02-16 23:31:39 +0200

    Test update

--- test.py
+++ test.py
@@ -1,17 +1,2 @@
 
-def test(mainCity):
-    titleDict = {
-        'design': '🥇 Web Design Agency in ' + mainCity + '. Web designers in ' + mainCity,
-        'development': '🥇 Web Development Agency in ' + mainCity + '. Web developers in ' + mainCity,
-        'magento': '🥇 Magento Web Development & eCommerce consulting agency in ' + mainCity,
-        'shopify': '🥇 Shopify Development Agency in ' + mainCity + '. Web developers in ' + mainCity,
-        'wordpress': '🥇 WordPress & WooCommerce Development Agency in ' + mainCity + '. Web developers in ' + mainCity
-    }
-    print(titleDict['design'])
-
-
-cities = ['ONE', 'TWO', 'THREE']
-
-
-for city in cities:
-    test(city)
+# Here I test all code
